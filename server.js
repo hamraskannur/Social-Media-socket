2f3a0e25cf0ccd724af73bd47cd0f84aa0876ee4
hamraskannur <kkhamras741@gmail.com>
2023-02-09 19:03:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const io = require("socket.io")(8800, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+let activeUser = [];
+io.on("connection", (socket) => {
+  socket.on("new-user-add", (newUserId) => { 
+    if (!activeUser.some((user) => user.userId === newUserId)) {
+      activeUser.push({
+        userId: newUserId,
+        socketId: socket.id,
+      });
+    }    
+    io.emit("get-user", activeUser);
+  });
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    const user = activeUser.find((user) => user.userId === receiverId);
+    if (user) {
+      io.to(user.socketId).emit("receive-message",data);
+    }
+  });
+  socket.on("disconnect", () => {
+    activeUser = activeUser.filter((user) => user.socketId !== socket.id);
+    io.emit("get-user", activeUser);
+  });
+});
